627ec237ebfc62c175aac15891fd965ad918c0b1
Miruna Dumitrascu <miru_dumi@yahoo.com>
2017-05-18 12:30:52 +0100

    Increased the size of the array
    to accomodate all chars and not just letters

--- Q1.java
+++ Q1.java
@@ -1,7 +1,7 @@
 public class Q1{
 
 	public static void main(String[] args){
-		System.out.println(fun(args[0], args[1]));
+		System.out.println(fun("1", ""));
 	}
 	
 	public static boolean fun(String s1, String s2){
@@ -13,14 +13,14 @@ public class Q1{
 		s1 = s1.toLowerCase();
 		s2 = s2.toLowerCase();	
 
-		int[] v = new int[26];	
+		int[] v = new int[130];	// changed to 130 to allow all ASCII chars to be represented
 
 		for(int i = 0; i < l1; i++)
-			v[s1.charAt(i) - 'a'] ++;
+			v[s1.charAt(i)] ++;
 
 		for(int i = 0; i < l2; i++)
-			if(v[s2.charAt(i) - 'a'] != 0)
-				v[s2.charAt(i) - 'a'] --;
+			if(v[s2.charAt(i)] != 0)
+				v[s2.charAt(i)] --;
 			else return false;
 			
 		return true;
